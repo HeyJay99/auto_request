f0b3d5827bbdaf8b4ce8ee81f4e5f7eb7c8cd989
HeyJay99 <woojy0513@gmail.com>
2025-07-16 16:49:47 +0900

    작업 완료 메세지 출력 오류 수정

--- Desktop/auto/auto.py
+++ Desktop/auto/auto.py
@@ -5,23 +5,31 @@ import keyboard
 
 stop = False
 
+#중단 함수 생성
 def stop_macro():
     global stop
     stop = True
     print("Q 키 감지됨: 자동화 중단 요청됨")
 
+#키보드 q를 눌렀을때 중단 함수가 호출 될수 있도록 설정
 keyboard.add_hotkey('q', stop_macro)
 
-df = pd.read_excel("C:/Users/MyCom/Desktop/auto/자재업로드.xlsx", sheet_name="3층", skiprows=2)
+#데이터 프레임 불러오기, 시트와 읽어올 행 설정
+df = pd.read_excel("C:/Users/MyCom/Desktop/auto/자재업로드.xlsx", sheet_name="2층", skiprows=2)
 
+#매크로 반복문 설정
 for index, row in df.iterrows():
+
+    #매크로로 인해 중단될 경우 메세지 출력 후 중단
     if stop:
         print("자동화 수동 중단됨.")
         break
 
+    #아이템 코드 읽어오기 + 공백제거
     item_code = str(row['일흥품번']).strip()
     
-    if pd.isna(row['일흥품번']):
+    #아이템 코드를 다읽고 빈칸이 나오면 프로그램 종료
+    if item_code == None:
         print("작업 완료")
         break
 
